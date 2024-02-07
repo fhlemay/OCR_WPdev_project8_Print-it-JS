174ed5fdea0464e84b339c6a1b6afaecd4e0bfc9
flemay <flemay@gmail.com>
2024-02-07 16:06:08 +0100

    [Étape 3] Ajouter les bullet points au slider

--- assets/script.js
+++ assets/script.js
@@ -17,12 +17,14 @@ const slides = [
 	}
 ]
 
+/* 
+ * Slider's arrows 
+ * */
 let bannerArrowLeft = document.getElementById("banner_arrow_left");
 let bannerArrowRight= document.getElementById("banner_arrow_right");
 
-
-bannerArrowLeft.onmouseover = null;
-bannerArrowRight.onmouseover = null;
+// bannerArrowLeft.onmouseover = null;
+// bannerArrowRight.onmouseover = null;
 
 bannerArrowLeft.onclick = function () {
   console.log(bannerArrowLeft.getAttribute("alt"));  
@@ -32,3 +34,20 @@ bannerArrowRight.onclick = function () {
   console.log(bannerArrowRight.getAttribute("alt"));  
 }
 
+/*
+ * Slider's dots
+ * */
+
+let currentSlide = 0;
+
+let dots = document.getElementById("banner_dots");
+console.log(dots);
+
+for (let i = 0; i < slides.length; i++) {
+  let dot = document.createElement("div");  
+  if (i == currentSlide) {
+    dot.classList.add("dot_selected");
+  }
+  dot.classList.add("dot");
+  dots.appendChild(dot);
+}
